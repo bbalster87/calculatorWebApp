b7c550b66ed56573af2e4e9a5defad853781ddeb
Bradley Balster <bbalster87@gmail.com>
2022-08-26 19:27:54 +0300

    Add display box and buttons with styling

--- app.js
+++ app.js
@@ -1,2 +1,35 @@
+// Get our base container to hold the calculator
 const containerDiv = document.querySelector(".container");
 
+// Create our input/output display
+const displayDiv = document.createElement("div");
+displayDiv.classList.add("display");
+containerDiv.appendChild(displayDiv)
+
+function buildButtons() {
+  const layout = [
+    ["7", "8", "9", "/"],
+    ["4", "5", "6", "*"],
+    ["1", "2", "3", "-"],
+    ["0", ".", "+", "←"],
+    ["Clear"]
+  ];
+  layout.forEach(buttons => {
+    const row = document.createElement("div");
+    row.classList.add("row");
+    buttons.forEach(button => {
+      const calcButton = document.createElement("button");
+      calcButton.textContent = button;
+      calcButton.classList.add("inputButton");
+      calcButton.addEventListener("click", receiveInput);
+      row.appendChild(calcButton);
+    });
+    containerDiv.appendChild(row);
+  });
+}
+
+function receiveInput() {
+  console.log(this.textContent)
+}
+
+buildButtons()
